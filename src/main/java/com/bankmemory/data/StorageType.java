99f98b0b20359aec7637487457c22057e68e9515
Matt <matthew.finney@hotmail.com>
2025-09-19 22:50:43 -0400

    Add StorageType enum to define storage options for bank and seed vault

---
+++ src/main/java/com/bankmemory/data/StorageType.java
@@ -0,0 +1,18 @@
+package main.java.com.bankmemory.data;
+
+public enum StorageType {
+    BANK,
+    SEED_VAULT;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case BANK:
+                return "Bank";
+            case SEED_VAULT:
+                return "Seed Vault";
+            default:
+                return name();
+        }
+    }
+}
