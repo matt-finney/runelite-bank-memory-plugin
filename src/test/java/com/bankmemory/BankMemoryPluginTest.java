6fe89b143e4f27d3fb26f062786da123549fe9a5
Matt <matthew.finney@hotmail.com>
2025-09-20 09:44:11 -0400

    Enhance BankMemoryPluginTest with additional mock setups for seed vault and bank panels

--- src/test/java/com/bankmemory/BankMemoryPluginTest.java
+++ src/test/java/com/bankmemory/BankMemoryPluginTest.java
@@ -1,6 +1,15 @@
 package com.bankmemory;
 
 import com.bankmemory.data.PluginDataStore;
+import com.bankmemory.BankDiffPanel;
+import com.bankmemory.BankMemoryItemOverlay;
+import com.bankmemory.BankSavesTopPanel;
+import com.bankmemory.SeedVaultSavesTopPanel;
+import com.bankmemory.CurrentSeedVaultPanelController;
+import com.bankmemory.SavedSeedVaultPanelController;
+import com.bankmemory.SeedVaultDiffPanelController;
+import com.bankmemory.bankview.BankViewPanel;
+import com.bankmemory.bankview.BankViewPanel;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -35,11 +44,21 @@ public class BankMemoryPluginTest {
     @Mock @Bind private PluginDataStore pluginDataStore;
     @Mock @Bind private BankMemoryConfig bankMemoryConfig;
     @Mock @Bind private OverlayManager overlayManager;
+    @Mock @Bind private BankMemoryItemOverlay itemOverlay;
     @Mock private CurrentBankPanelController currentBankPanelController;
+    @Mock private CurrentSeedVaultPanelController currentSeedVaultPanelController;
     @Mock private SavedBanksPanelController savedBanksPanelController;
+    @Mock private SavedSeedVaultPanelController savedSeedVaultPanelController;
     @Mock private BankDiffPanelController bankDiffPanelController;
+    @Mock private SeedVaultDiffPanelController seedVaultDiffPanelController;
     @Mock private BankMemoryPluginPanel pluginPanel;
     @Mock private Injector pluginInjector;
+    @Mock private BankSavesTopPanel bankSavesTopPanel;
+    @Mock private SeedVaultSavesTopPanel seedVaultSavesTopPanel;
+    @Mock private BankDiffPanel banksDiffPanel;
+    @Mock private BankDiffPanel seedVaultDiffPanel;
+    @Mock private BankViewPanel currentSeedVaultViewPanel;
+    @Mock private BankViewPanel currentBankViewPanel;
 
     @Inject private TestBankMemoryPlugin bankMemoryPlugin;
 
@@ -49,9 +68,21 @@ public class BankMemoryPluginTest {
         bankMemoryPlugin.setInjector(pluginInjector);
         when(pluginInjector.getInstance(BankMemoryPluginPanel.class)).thenReturn(pluginPanel);
         when(pluginInjector.getInstance(CurrentBankPanelController.class)).thenReturn(currentBankPanelController);
+        when(pluginInjector.getInstance(CurrentSeedVaultPanelController.class)).thenReturn(currentSeedVaultPanelController);
         when(pluginInjector.getInstance(SavedBanksPanelController.class)).thenReturn(savedBanksPanelController);
+        when(pluginInjector.getInstance(SavedSeedVaultPanelController.class)).thenReturn(savedSeedVaultPanelController);
         when(pluginInjector.getInstance(BankDiffPanelController.class)).thenReturn(bankDiffPanelController);
-        when(pluginPanel.getSavedBanksTopPanel()).thenReturn(mock(BankSavesTopPanel.class));
+        when(pluginInjector.getInstance(SeedVaultDiffPanelController.class)).thenReturn(seedVaultDiffPanelController);
+
+        // Panels returned from pluginPanel
+        when(pluginPanel.getSavedBanksTopPanel()).thenReturn(bankSavesTopPanel);
+        when(pluginPanel.getSavedSeedVaultTopPanel()).thenReturn(seedVaultSavesTopPanel);
+        when(pluginPanel.getCurrentSeedVaultViewPanel()).thenReturn(currentSeedVaultViewPanel);
+        when(pluginPanel.getCurrentBankViewPanel()).thenReturn(currentBankViewPanel);
+
+        // Diff panels from top panels
+        when(bankSavesTopPanel.getDiffPanel()).thenReturn(banksDiffPanel);
+        when(seedVaultSavesTopPanel.getDiffPanel()).thenReturn(seedVaultDiffPanel);
     }
 
     @Test
